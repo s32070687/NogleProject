3c3add7e68c225d6b5e7910dc51ad546370ab60c
Jason <Jason.Lai@ehsn.com.tw>
2023-07-08 19:06:42 +0800

    add library navigation remove glide

--- app/src/main/java/com/eulsapet/nogleproject/view/fragment/FragmentD.kt
+++ app/src/main/java/com/eulsapet/nogleproject/view/fragment/FragmentD.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.eulsapet.nogleproject.R
 import com.eulsapet.nogleproject.databinding.FragmentDBinding
 
 class FragmentD: Fragment() {
@@ -27,6 +29,7 @@ class FragmentD: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.ibSetting.setOnClickListener {
+            val navController = findNavController()
         }
     }
 
