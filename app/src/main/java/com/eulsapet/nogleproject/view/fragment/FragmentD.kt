088cda6ce338e14893c2f96414bbf40cb297ca1e
Jason <Jason.Lai@ehsn.com.tw>
2023-07-08 23:05:30 +0800

    add nav

--- app/src/main/java/com/eulsapet/nogleproject/view/fragment/FragmentD.kt
+++ app/src/main/java/com/eulsapet/nogleproject/view/fragment/FragmentD.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import com.eulsapet.nogleproject.R
 import com.eulsapet.nogleproject.databinding.FragmentDBinding
@@ -29,7 +30,7 @@ class FragmentD: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.ibSetting.setOnClickListener {
-            val navController = findNavController()
+            Navigation.findNavController(it).navigate(R.id.action_fg_d_to_fg_setting)
         }
     }
 
